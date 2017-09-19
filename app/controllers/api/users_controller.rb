0ba3ab8ea9ed83bea75412b95bf923e8766cb65c
Logan Cooper <ldtcoop@gmail.com>
2017-09-19 14:40:11 -0700

    Add users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,21 @@
 class Api::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render :create
+    else
+      render json: @user.errors.full_messages
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
