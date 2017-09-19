dcdb0706a2f8a6f12cacc99d141f0b2427c5e453
Logan Cooper <ldtcoop@gmail.com>
2017-09-19 16:24:55 -0700

    Create Jbuilder views

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -6,7 +6,7 @@ class Api::SessionsController < ApplicationController
     )
     if @user
       login!(@user)
-      render 'api/users/create'
+      render 'api/users/show'
     else
       render json: ['Invalid username or password'], status: 422
     end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -7,7 +7,7 @@ class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render :create
+      render :show
     else
       render json: @user.errors.full_messages
     end
