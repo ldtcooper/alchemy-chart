999f2a3a17d616c1823c316727d556e681617d5f
Logan Cooper <ldtcoop@gmail.com>
2017-09-19 20:07:47 -0700

    Add null user default to session reducer

--- frontend/reducers/session_reducers.js
+++ frontend/reducers/session_reducers.js
@@ -1,11 +1,13 @@
 import { RECEIVE_CURRENT_USER } from '../actions/session_action';
 
-const SessionReducer = (state = {}, action) => {
+const _nullUser = Object.freeze({currentUser: null});
+
+const SessionReducer = (state = _nullUser, action) => {
   Object.freeze(state);
   let newState = Object.assign({}, state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      newState = action.user;
+      newState.currentUser = action.user;
       return newState;
     default:
       return state;
