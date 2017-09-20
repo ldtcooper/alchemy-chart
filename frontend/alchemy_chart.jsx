006bbf0104bd8093c0db55278a1288986034bcba
Logan Cooper <ldtcoop@gmail.com>
2017-09-19 21:27:40 -0700

    Refactor entry file to use Root

--- frontend/alchemy_chart.jsx
+++ frontend/alchemy_chart.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as action from './actions/session_actions';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
-  ReactDOM.render(<h1>Alchemy Chart</h1>, root);
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.login = action.login;
-  window.signup = action.signup;
-  window.logout = action.logout;
-
+  ReactDOM.render(<Root store = {store} />, root);
 });
