c7fba0363314949d6986d991a9de4ccaa63f3e27
Logan Cooper <ldtcoop@gmail.com>
2017-09-19 17:45:48 -0700

    Create API utils for user auth

--- frontend/alchemy_chart.jsx
+++ frontend/alchemy_chart.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as sessionApi from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Alchemy Chart</h1>, root);
+  window.login = sessionApi.login;
+  window.signup = sessionApi.signup;
+  window.logout = sessionApi.logout;
 });
