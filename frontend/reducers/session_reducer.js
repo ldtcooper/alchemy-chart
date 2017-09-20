8399483ae3e93f3b1db5173c921e165669c5bef8
Logan Cooper <ldtcoop@gmail.com>
2017-09-19 21:03:17 -0700

    Assign currentUser to action.currentUser

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,14 +1,12 @@
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 
-const _nullUser = Object.freeze({currentUser: null});
 
-const SessionReducer = (state = _nullUser, action) => {
+const SessionReducer = (state = {currentUser: null}, action) => {
   Object.freeze(state);
   let newState = Object.assign({}, state);
-  console.log(action);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      newState.currentUser = action.user;
+      newState.currentUser = action.currentUser;
       return newState;
     default:
       return state;
