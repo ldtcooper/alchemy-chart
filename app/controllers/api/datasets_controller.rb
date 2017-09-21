25d22431a4a8454b115aeb4f8ff37b115045759c
Logan Cooper <ldtcoop@gmail.com>
2017-09-21 11:50:29 -0700

    Finish datasets controller

--- app/controllers/api/datasets_controller.rb
+++ app/controllers/api/datasets_controller.rb
@@ -1,13 +1,32 @@
 class Api::DatasetsController < ApplicationController
   def index
-
+    @datasets = Dataset.find_by(current_user.id)
+    render :index
   end
 
   def create
-
+    @dataset = Dataset.new(dataset_params)
+    @dataset.owner_id = current_user.id
+    if @dataset.save
+      render :index
+    else
+      render json: @dataset.errors.full_messages, status: 422
+    end
   end
 
   def destroy
+    @dataset = Dataset.find(params[:id])
+    if @dataset
+      @dataset.delete
+      render :index
+    else
+      render json: ["No such dataset found"], status: 404
+    end
+  end
 
+  private
+
+  def dataset_params
+    params.require(:dataset).permit(:dataset_name, :data_type, :data_text)
   end
 end
