17bb821463bafd9d4021aec2aae56cc46262ca90
Logan Cooper <ldtcoop@gmail.com>
2017-09-21 14:39:55 -0700

    Add AJAX calls for datasets

---
+++ frontend/util/dataset_api_util.js
@@ -0,0 +1,21 @@
+export const fetchDatasets = () => (
+  $.ajax({
+    method: 'GET',
+    url: 'api/datasets'
+  })
+);
+
+export const sendDataset = (set) => (
+  $.ajax({
+    method: 'POST',
+    url: 'api/datasets',
+    data: { set }
+  })
+);
+
+export const deleteDataset = (id) => (
+  $.ajax({
+    method: 'DELETE',
+    url: `api/datasets/${id}`
+  })
+);
