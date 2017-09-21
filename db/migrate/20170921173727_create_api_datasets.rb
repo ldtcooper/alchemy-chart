0c9ee9646d578957bdbeb45f15b97391a0860e57
Logan Cooper <ldtcoop@gmail.com>
2017-09-21 10:43:45 -0700

    Recreate datasets model under correct namespace

--- db/migrate/20170921173727_create_api_datasets.rb
+++ db/migrate/20170921173727_create_api_datasets.rb
@@ -1,9 +1,10 @@
-class CreateDatasets < ActiveRecord::Migration[5.1]
+class CreateApiDatasets < ActiveRecord::Migration[5.1]
   def change
+    drop_table :datasets
     create_table :datasets do |t|
       t.integer :owner_id, null: false
       t.string :dataset_name, null: false
-      t.string :data_type, null: false 
+      t.string :data_type, null: false
       t.jsonb :data_text, null: false
       t.timestamps
     end
--- test/models/api/dataset_test.rb
+++ test/models/api/dataset_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class DatasetTest < ActiveSupport::TestCase
+class Api::DatasetTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
