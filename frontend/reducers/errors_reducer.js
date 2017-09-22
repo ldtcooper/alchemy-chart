2f72abd3270ba7a6962e39cd6e0e80f0ef0a82b7
Logan Cooper <ldtcoop@gmail.com>
2017-09-21 17:39:56 -0700

    Integrate new reducers

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,8 +1,10 @@
 import SessionErrorsReducer from './session_errors_reducer';
+import DatasetErrorsReducer from './dataset_errors_reducer';
 import {combineReducers} from 'redux';
 
 const ErrorsReducer = combineReducers({
-  session: SessionErrorsReducer
+  session: SessionErrorsReducer,
+  dataUpload: DatasetErrorsReducer
 });
 
 export default ErrorsReducer;
