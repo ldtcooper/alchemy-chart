57912f8bdc3e29c5b6e774cb306aacbdefd79d69
Logan Cooper <ldtcoop@gmail.com>
2017-09-21 21:33:49 -0700

    Add /datasets route to mainpage element

--- frontend/components/main_page.jsx
+++ frontend/components/main_page.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SignupContainer from './signup/signup_container';
+import DatasetListContainer from './datasets/dataset_list_container';
 import { Route, Switch } from 'react-router-dom';
 
 const MainPage = () => (
   <main>
     <Switch>
       <Route exact path="/" render={() => <SignupContainer /> } />
+      <Route exact path="/datasets" render={() => <DatasetListContainer /> } />
     </Switch>
   </main>
 );
