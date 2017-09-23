be014a6e8b37c7f09817885390001571c6ce1c49
Logan Cooper <ldtcoop@gmail.com>
2017-09-23 15:12:45 -0700

    Add helper method to determine file type

--- frontend/components/upload/data_upload.jsx
+++ frontend/components/upload/data_upload.jsx
@@ -12,7 +12,7 @@ class DataUpload extends React.Component {
     this.state = {
       disabled: false,
       name: "",
-      data_type: "",
+      dataType: "",
       dataset: [],
       errors: this.errors,
       message: "Drop your file here, or click to select file to upload.(CSV, TSV, and JSON files only)"
@@ -23,15 +23,29 @@ class DataUpload extends React.Component {
     this.fileReader = new FileReader();
   }
 
+  fileType(file) {
+    switch (file.type) {
+      case 'application/json':
+        return 'json';
+      case 'text/csv':
+        return 'csv';
+      case 'text/tab-separated-values':
+        return 'tsv';
+    }
+  }
+
   handleDrop(files) {
     const file = files[0]; //only allows one file to be uploaded at a time.
     const acceptedTypes = ['application/json', 'text/csv', 'text/tab-separated-values'];
+    const dataType = this.fileType(file);
+
     if (file.size > 20000) {
       this.setState({errors: ["Files must be under 20 kB in size."]});
     } else if (!acceptedTypes.include(file.type)) {
       this.setState({errors: ["Files must be CSV, TSV, or JSON"]});
     }
     else {
+      this.setState({errors: [], message: `${file.name}`}, dataType);
       this.fileReader.onload = (e) => {
         // parse file
       };
