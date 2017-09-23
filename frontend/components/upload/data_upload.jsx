398bd3df3865393bb68220a71c222499f3b73e5e
Logan Cooper <ldtcoop@gmail.com>
2017-09-23 14:49:57 -0700

    Add dummy handle drop function

--- frontend/components/upload/data_upload.jsx
+++ frontend/components/upload/data_upload.jsx
@@ -4,20 +4,34 @@ import Dropzone from 'react-dropzone';
 class DataUpload extends React.Component {
   constructor(props) {
     super(props);
+
+    if (this.errors === undefined) {
+      this.errors = [];
+    }
+
     this.state = {
       disabled: false,
-      files: [],
       name: "",
+      data_type: "",
+      dataset: [],
+      errors: this.errors,
       message: "Drop your file here, or click to select file to upload.(CSV, TSV, and JSON files only)"
       };
   }
 
+  componentDidMount() {
+    this.fileReader = new FileReader();
+  }
+
   handleDrop(files) {
-    this.setState({
-      disabled: true,
-      files,
-      message: 
-    });
+    const file = files[0]; //only allows one file to be uploaded at a time.
+    if (file.size > 20000) {
+      this.setState({errors: ["Files must be under 20 kB in size."]});
+    } else {
+      this.fileReader.onload = (e) => {
+        // parse file
+      };
+    }
   }
 
   render() {
@@ -28,6 +42,7 @@ class DataUpload extends React.Component {
           <Dropzone
             onDrop={this.handleDrop.bind(this)}
             disabled={this.state.disabled}
+            accept="application/json,text/csv,text/tab-separated-values"
             >
             <p>{this.state.message}</p>
           </Dropzone>
