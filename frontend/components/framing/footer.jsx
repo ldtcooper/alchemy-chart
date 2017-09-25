1cd1d16e1b25f9e27b6e9fa0818522bde58994a2
Logan Cooper <ldtcoop@gmail.com>
2017-09-25 16:27:16 -0700

    Make footer links open in new tab

--- frontend/components/framing/footer.jsx
+++ frontend/components/framing/footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => (
   <footer>
     <div className='footer-content'>
       <div className="links">
-        <a href="https://www.linkedin.com/in/logandtcooper/"><i className="fa fa-linkedin-square"></i></a>
-        <a href="https://github.com/ldtcooper"><i className="fa fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/logandtcooper/" target="_blank"><i className="fa fa-linkedin-square"></i></a>
+        <a href="https://github.com/ldtcooper" target="_blank"><i className="fa fa-github"></i></a>
       </div>
       <div className="created-by">
         <p>Created by Logan Cooper -- ldtcoop@gmail.com</p>
