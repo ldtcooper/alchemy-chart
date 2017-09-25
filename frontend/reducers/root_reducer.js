3e368c39da07f18136b5e02186d63494c6d4a55c
Logan Cooper <ldtcoop@gmail.com>
2017-09-24 22:34:49 -0700

    Integrate current chart reducer into root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,11 +3,13 @@ import SessionReducer from './session_reducer';
 import ErrorsReducer from './errors_reducer';
 import DatasetsReducer from './dataset_reducer';
 import ChartsReducer from './charts_reducer';
+import CurrentChartReducer from './current_chart_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,
   datasets: DatasetsReducer,
   charts: ChartsReducer,
+  currentChart: CurrentChartReducer,
   errors: ErrorsReducer
 });
 
