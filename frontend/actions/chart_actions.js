134d8f20d358a5224387aa0c527b7a197d7cca47
Logan Cooper <ldtcoop@gmail.com>
2017-09-24 21:56:58 -0700

    Add basic chart actions

--- frontend/actions/chart_actions.js
+++ frontend/actions/chart_actions.js
@@ -3,3 +3,24 @@ import * as ChartApiUtils from '../util/dataset_api_util';
 export const RECEIVE_CHARTS = 'RECEIVE_CHARTS';
 export const RECEIVE_CHART = 'RECEIVE_CHART';
 export const RECEIVE_CHART_ERRORS = 'RECEIVE_CHART_ERRORS';
+
+const receiveCharts = (charts) => {
+  return {
+    type: RECEIVE_CHARTS,
+    charts
+  };
+};
+
+const receiveChart = (chart) => {
+  return {
+    type: RECEIVE_CHART,
+    chart
+  };
+};
+
+const receiveErrors = (errors) => {
+  return {
+    type: RECEIVE_CHART_ERRORS,
+    errors
+  };
+};
