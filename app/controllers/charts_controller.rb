2993bb13359f03d555425279c8a51fa3b8936342
Logan Cooper <ldtcoop@gmail.com>
2017-09-24 18:24:22 -0700

    Create chart controller skeleton

---
+++ app/controllers/charts_controller.rb
@@ -0,0 +1,22 @@
+class ChartsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+end
+
+# api_charts GET    /api/charts(.:format)       api/charts#index
+#            POST   /api/charts(.:format)       api/charts#create
+#  api_chart GET    /api/charts/:id(.:format)   api/charts#show
+#            DELETE /api/charts/:id(.:format)   api/charts#destroy
