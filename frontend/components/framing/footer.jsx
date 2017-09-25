6130f2c8ed84079fd87ab34e0a7c0b748b0ddd2d
Logan Cooper <ldtcoop@gmail.com>
2017-09-25 11:08:28 -0700

    Restyle social media icons

--- frontend/components/framing/footer.jsx
+++ frontend/components/framing/footer.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 const Footer = () => (
   <footer>
-    <div className="links">
-      <a href="https://www.linkedin.com/in/logandtcooper/"><i className="fa fa-linkedin-square"></i></a>
-      <a href="https://github.com/ldtcooper"><i className="fa fa-github"></i></a>
-    </div>
-    <div className="created-by">
-      <p>Created by Logan Cooper -- ldtcoop@gmail.com</p>
+    <div className='footer-content'>
+      <div className="links">
+        <a href="https://www.linkedin.com/in/logandtcooper/"><i className="fa fa-linkedin-square"></i></a>
+        <a href="https://github.com/ldtcooper"><i className="fa fa-github"></i></a>
+      </div>
+      <div className="created-by">
+        <p>Created by Logan Cooper -- ldtcoop@gmail.com</p>
+      </div>
     </div>
   </footer>
 );
