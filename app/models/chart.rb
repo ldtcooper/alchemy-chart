e54b12ec5702401a1a90f1ca57fb030892143795
Logan Cooper <ldtcoop@gmail.com>
2017-09-24 18:13:40 -0700

    Validate inputs in chart model

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: charts
+#
+#  id         :integer          not null, primary key
+#  owner_id   :integer          not null
+#  dataset_id :integer          not null
+#  chart_type :string           not null
+#  chart_sort :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Chart < ApplicationRecord
+  validates :owner_id, :dataset_id, presence: true
+  validates :chart_type, presence: true, inclusion: { in: ['circle', 'bar', 'line', 'plot']}
+  validates :chart_sort, presence: true, inclusion: { in: ['y-asc', 'y-desc', 'x-asc', 'x-desc']}
+
+
 end
--- test/models/chart_test.rb
+++ test/models/chart_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: charts
+#
+#  id         :integer          not null, primary key
+#  owner_id   :integer          not null
+#  dataset_id :integer          not null
+#  chart_type :string           not null
+#  chart_sort :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class ChartTest < ActiveSupport::TestCase
