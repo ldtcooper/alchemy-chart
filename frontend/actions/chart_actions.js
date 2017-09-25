2497e17152ad456d457caaea5b248a034c266f89
Logan Cooper <ldtcoop@gmail.com>
2017-09-24 22:04:41 -0700

    Write thunk action creators for charts

--- frontend/actions/chart_actions.js
+++ frontend/actions/chart_actions.js
@@ -24,3 +24,35 @@ const receiveErrors = (errors) => {
     errors
   };
 };
+
+export const seeAllCharts = () => dispatch => {
+  ChartApiUtils.fetchCharts()
+  .then(
+    (charts) => dispatch(receiveCharts(charts)),
+    (err) => dispatch(receiveErrors(err))
+  );
+};
+
+export const seeOneChart = (id) => dispatch => {
+  ChartApiUtils.fetchChart(id)
+  .then(
+    (chart) => dispatch(receiveChart(chart)),
+    (err) => dispatch(receiveErrors(err))
+  );
+};
+
+export const addChart = (chart) => dispatch => {
+  ChartApiUtils.createChart(chart).
+  then(
+    null,
+    (err) => dispatch(receiveErrors(err))
+  );
+};
+
+export const deleteChart = (id) => dispatch => {
+  ChartApiUtils.deleteChart(id)
+  .then(
+    null,
+    (err) => dispatch(receiveErrors(err))
+  );
+};
