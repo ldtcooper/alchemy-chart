1019af94630d3eb2b4a43bf7698ddcc14e52b365
Logan Cooper <ldtcoop@gmail.com>
2017-09-25 15:05:02 -0700

    Write chart list container

---
+++ frontend/components/chart/chart_list_container.jsx
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import {
+  seeAllCharts, deleteChart
+} from '../../actions/';
+import ChartList from './chart_list';
+
+const mapStateToProps = (state) => ({
+  charts: state.charts
+});
+
+const mapDispatchToProps = dispatch => ({
+  seeAllCharts: () => dispatch(seeAllCharts()),
+  deleteChart: (id) => dispatch(deleteChart(id))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ChartList);
