f2949991700102c7984579131ea6b45541901b44
Logan Cooper <ldtcoop@gmail.com>
2017-09-28 13:54:25 -0700

    Make share page skeleton

--- frontend/components/shares/share_page.jsx
+++ frontend/components/shares/share_page.jsx
@@ -1,1 +1,24 @@
-import { connect } from 'react-redux';
+import React from 'react';
+
+class SharePage {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return(
+      <div className='share-form'>
+        <form>
+          <select>
+
+          </select>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SharePage;
