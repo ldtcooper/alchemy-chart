a4b8b135d7d04f672d5705e0b0ececea127e7796
Logan Cooper <ldtcoop@gmail.com>
2017-09-28 13:45:44 -0700

    Add shares container

---
+++ frontend/components/shares/shares_container.jsx
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import { shareChart } from '../../actions/share_actions';
+import { fetchUsers } from '../../actions/user_actions';
+import SharePage from './share_page';
+
+const mapStateToProps = (state) => ({
+  users: state.users
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  shareChart: (userId, chartId) => (dispatch(shareChart(userId, chartId))),
+  fetchUsers: () => (dispatch(fetchUsers()))
+});
