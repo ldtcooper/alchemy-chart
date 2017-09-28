59f0eff8af0626a18ce2c72cf2883b2a5594f782
Logan Cooper <ldtcoop@gmail.com>
2017-09-27 22:41:22 -0700

    Build shares controller

--- app/controllers/api/shares_controller.rb
+++ app/controllers/api/shares_controller.rb
@@ -1,2 +1,16 @@
 class Api::SharesController < ApplicationController
+  def create
+    @share = Share.new(share_params)
+    if @share.save
+      render json: ["Success"]
+    else
+      render json: @share.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def share_params
+    params.require(:share).permit(:sharee_id, :chart_id)
+  end
 end
