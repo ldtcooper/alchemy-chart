b7802fbea4273e5e2c5250555c4fc354963e1122
Logan Cooper <ldtcoop@gmail.com>
2017-09-29 10:53:53 -0700

    Prepare for production mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,19 @@
 var path = require("path");
 var webpack = require("webpack");
 
-var plugins = []; // if using any plugins for both dev and production
-var devPlugins = []; // if using any plugins for development
-
-var prodPlugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
-];
-
-plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-);
-
 module.exports = {
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
   context: __dirname,
   entry: './frontend/alchemy_chart.jsx',
   output: {
